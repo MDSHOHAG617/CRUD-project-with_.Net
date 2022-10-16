80622cfac504b6b7c888aaa5c4167a97c3c9fe7b
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-10-16 23:21:56 +0600

    Drawing fun game added!

--- first tesy app/mainFrame.Designer.cs
+++ first tesy app/mainFrame.Designer.cs
@@ -28,28 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainFrame));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.age = new System.Windows.Forms.TextBox();
+            this.Insert = new System.Windows.Forms.Button();
+            this.update = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.search = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(220, 40);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label1.Location = new System.Drawing.Point(236, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(285, 24);
+            this.label1.Size = new System.Drawing.Size(210, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "WELCOME TO MAIN FRAME";
+            this.label1.Text = "WLCOME TO MAIN FRAME";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label2.Location = new System.Drawing.Point(148, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(410, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Insert Update Delete With SQL Server";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(190, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ID";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label4.Location = new System.Drawing.Point(181, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label5.Location = new System.Drawing.Point(190, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Age";
+            // 
+            // id
+            // 
+            this.id.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.id.Location = new System.Drawing.Point(257, 95);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(223, 26);
+            this.id.TabIndex = 5;
+            this.id.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(257, 137);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(223, 26);
+            this.name.TabIndex = 6;
+            // 
+            // age
+            // 
+            this.age.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.age.Location = new System.Drawing.Point(257, 175);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(223, 26);
+            this.age.TabIndex = 7;
+            // 
+            // Insert
+            // 
+            this.Insert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Insert.Location = new System.Drawing.Point(153, 221);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(77, 37);
+            this.Insert.TabIndex = 8;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = false;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // update
+            // 
+            this.update.BackColor = System.Drawing.Color.Cyan;
+            this.update.Location = new System.Drawing.Point(257, 221);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(77, 37);
+            this.update.TabIndex = 9;
+            this.update.Text = "Update";
+            this.update.UseVisualStyleBackColor = false;
+            this.update.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // delete
+            // 
+            this.delete.BackColor = System.Drawing.Color.Red;
+            this.delete.Location = new System.Drawing.Point(351, 221);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(77, 37);
+            this.delete.TabIndex = 10;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = false;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // search
+            // 
+            this.search.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.search.Location = new System.Drawing.Point(450, 221);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(77, 37);
+            this.search.TabIndex = 11;
+            this.search.Text = "Serch";
+            this.search.UseVisualStyleBackColor = false;
+            this.search.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(153, 264);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(374, 150);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Turquoise;
+            this.linkLabel1.Location = new System.Drawing.Point(162, 441);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(365, 24);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Feeling Bored? let\'s have fun Drawing";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // mainFrame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Violet;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(676, 504);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "mainFrame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "mainFrame";
+            this.Load += new System.EventHandler(this.mainFrame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +237,18 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
--- first tesy app/mainFrame.cs
+++ first tesy app/mainFrame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,9 +18,93 @@ namespace first_tesy_app
             InitializeComponent();
         }
 
+        private void mainFrame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=MD_SHOHAG\\SQLEXPRESS01;Initial Catalog=CRUD_table;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into UserCRUD_Table values (@ID,@Name,@Age)", con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(id.Text));
+            cmd.Parameters.AddWithValue("@Name", (name.Text));
+            cmd.Parameters.AddWithValue("@Age", double.Parse(age.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully data inserted");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=MD_SHOHAG\\SQLEXPRESS01;Initial Catalog=CRUD_table;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update UserCRUD_Table set Name = @Name, Age=@Age where ID = @ID", con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(id.Text));
+            cmd.Parameters.AddWithValue("@Name", (name.Text));
+            cmd.Parameters.AddWithValue("@Age", double.Parse(age.Text));
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Successfully data Updated");
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=MD_SHOHAG\\SQLEXPRESS01;Initial Catalog=CRUD_table;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete UserCRUD_Table  where ID = @ID", con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(id.Text));
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Successfully data Deleted");
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=MD_SHOHAG\\SQLEXPRESS01;Initial Catalog=CRUD_table;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from UserCRUD_Table where ID = @ID", con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(id.Text));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            funDrawing funDrawing = new funDrawing();
+            funDrawing.Show();
+            Hide();
         }
     }
 }
